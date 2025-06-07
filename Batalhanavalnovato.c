224b5e3f8a3a688062ddd274e4080c87cd50512d
JeanM2121 <jean.mdossantos21@gmail.com>
2025-06-07 13:23:00 -0300

    Update Batalhanavalnovato.c

--- Batalhanavalnovato.c
+++ Batalhanavalnovato.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 
-//navio 1(Array inidimensional)
+//navio 1(Array Unidimensional).
 
 void Navio1(int tabuleiro[10][10], int x, int y, int tamanho){
     for (int i = 0; i < tamanho; i++){
         tabuleiro[x][y + i] = 1;
     }
 }
-// navio 2(Array unidimensional)
+// navio 2(Array unidimensional).
 void Navio2(int tabuleiro[10][10], int x, int y, int tamanho){
     for (int i = 0; i < tamanho; i++){
         tabuleiro[x + i][y] = 2;
     }
 }
 
-//Tabuleiro (Array bidimensional) com loops aninhados em FOR
+//Tabuleiro (Array bidimensional) com loops aninhados em FOR.
 
 void Tabuleiro(int tabuleiro[10][10]){
-    printf("\n");
-    printf("*** BATALHA NAVAL*** \n");
+    
+    printf("BATALHA NAVAL\n");
     printf("0 representa agua \n");
-    printf("1 representa Navio 1 \n");
-    printf("2 representa Navio 2 \n");
+    printf("Navio 1 \n");
+    printf("Navio 2 \n");
     printf("\n");
     printf("  A B C D E F G H I J \n");
     for (int i = 0; i < 10; i++){
@@ -36,8 +36,8 @@ void Tabuleiro(int tabuleiro[10][10]){
 //Posições dos navios 
 int main(){
     int tabuleiro[10][10] = {0};
-    Navio1(tabuleiro, 2, 3, 3);
-    Navio2(tabuleiro, 5, 6, 3);
+    Navio1(tabuleiro, 3, 3, 3); //Horizontal
+    Navio2(tabuleiro, 5, 5, 3); //Vertical
     Tabuleiro(tabuleiro);
 
     return 0;
